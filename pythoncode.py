f167d6f0524bfd30948b7c5dce7ee0acdde3d8bb
arockiadassdev <arockiadassdev@gmail.com>
2026-06-02 15:06:14 +0530

    commit message for your changes. Lines starting

--- pythoncode.py
+++ pythoncode.py
@@ -1,1 +1,11 @@
-print ("Hello word")
+import statistics
+
+data = [10, 20, 30, 40, 50, 20]
+
+mean = statistics.mean(data)
+median = statistics.median(data)
+mode = statistics.mode(data)
+
+print("Mean:", mean)
+print("Median:", median)
+print("Mode:", mode)
